3dfc8c212b63061dddee43e9c4baa2c579980ac6
Branelio <bjsangoluisa@espe.edu.ec>
2026-09-03 10:48:20 -0500

    feat: add Pixel ID 1138329888548484 to RootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -53,6 +53,7 @@ export default function RootLayout({
               s.parentNode.insertBefore(t,s)}(window, document,'script',
               'https://connect.facebook.net/en_US/fbevents.js');
               fbq('init', '1406230474804893');
+              fbq('init', '1138329888548484');
               fbq('track', 'PageView');
             `,
           }}
@@ -63,6 +64,13 @@ export default function RootLayout({
             width="1"
             style={{ display: 'none' }}
             src="https://www.facebook.com/tr?id=1406230474804893&ev=PageView&noscript=1"
+            alt=""
+          />
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=1138329888548484&ev=PageView&noscript=1"
             alt=""
           />
         </noscript>
